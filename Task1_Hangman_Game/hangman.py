356030b134be607665d6d25b348aae39e86cffa6
vidushivats24 <vidushivats2409@gmail.com>
2026-06-11 18:07:42 +0530

    Add Hangman Game Code

--- Task1_Hangman_Game/hangman.py
+++ Task1_Hangman_Game/hangman.py
@@ -1,1 +1,44 @@
+import random
 
+words = [
+    "python", "computer", "programming", "internship",
+    "developer", "github", "algorithm", "keyboard"
+]
+
+word = random.choice(words)
+guessed_letters = []
+attempts = 6
+
+print("🎮 Welcome to Hangman Game!")
+
+while attempts > 0:
+    display_word = ""
+
+    for letter in word:
+        if letter in guessed_letters:
+            display_word += letter + " "
+        else:
+            display_word += "_ "
+
+    print("\nWord:", display_word)
+
+    if "_" not in display_word:
+        print("\n🎉 Congratulations! You guessed the word:", word)
+        break
+
+    guess = input("Enter a letter: ").lower()
+
+    if guess in guessed_letters:
+        print("⚠ You already guessed that letter.")
+        continue
+
+    guessed_letters.append(guess)
+
+    if guess not in word:
+        attempts -= 1
+        print("❌ Wrong guess!")
+        print("Attempts left:", attempts)
+
+if attempts == 0:
+    print("\n💀 Game Over!")
+    print("The word was:", word)
